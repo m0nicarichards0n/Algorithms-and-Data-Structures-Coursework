8a7e0908a9aac3e10f18304641f1aa3480a219aa
monicarichardson <36137236+monicarichardson@users.noreply.github.com>
2020-03-06 02:51:52 +0000

    GITHUB ISSUE C41:  Display Board in Console
    Created Program, Board, Slot and Display classes.
    
    Board object has adjustable height and width properties as well as a dictionary of slots - key: board coordinates, value: slot object. Board x axis is alphabetical, y axis is numerical.
    
    Slot object has x and y coordinate properties, as well as a content property - 0 means slot is empty, 1 means slot is occupied by player 1 and 2 means slot is occupied by player 2.
    
    Display class prints formatted version of board object. Display class also includes welcome message, and messages to retrieve custom height and width between 1-26.

---
+++ ConnectFour/Board.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnectFour
+{
+    class Board
+    {
+        public IDictionary<string, Slot> Slots = new Dictionary<string, Slot>();
+        private int _height;
+        private int _width;
+
+        // Initialise empty board with custom height and width values (up to 26x26)
+        public void InitBoard(int height, int width)
+        {
+            if (height >=1 && height <=26
+                && width >= 1 && width <=26)
+            {
+                _height = height;
+                _width = width;
+
+                char[] xAxis = GetXAxis(width);
+                int[] yAxis = GetYAxis(height);
+
+                foreach (int y in yAxis)
+                {
+                    foreach (char x in xAxis)
+                    {
+                        Slot slot = new Slot(x, y, 0);
+                        Slots.Add(slot.XCoordinate.ToString() + slot.YCoordinate, slot);
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Please only enter dimensions up to 26x26");
+            }
+        }
+
+        // Return X axis of equivalent ASCII characters ('A' - 'Z') depending on chosen width
+        public char[] GetXAxis(int width)
+        {
+            char[] xAxis = new char[width];
+            for (int i = 0; i < width; i++)
+            {
+                xAxis[i] = (char)(i + 65);
+            }
+            return xAxis;
+        }
+
+        // Return Y axis of integers (1 - 26) depending on chosen height
+        public int[] GetYAxis(int height)
+        {
+            int[] yAxis = new int[height];
+            for (int i = 0; i < height; i++)
+            {
+                yAxis[i] = i + 1;
+            }
+
+            // Reverse Y axis array to ensure coordinates are stored in printing order
+            Array.Reverse(yAxis);
+
+            return yAxis;
+        }
+
+        public int Height
+        {
+            get => _height;
+        }
+
+        public int Width
+        {
+            get => _width;
+        }
+    }
+}
---
+++ ConnectFour/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConnectFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            Display display = new Display();
+            bool valid = true;
+
+            display.Welcome();
+
+            do
+            {
+                int height = display.GetHeight();
+                int width = display.GetWidth();
+
+                try
+                {
+                    board.InitBoard(height, width);
+                    display.PrintBoard(board);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    valid = false;
+                }
+
+            } while (!valid);
+        }
+    }
+}
